034abca444613107e99ec2a7206f71c558e9e4af
hieuemmm <hieus247@gmail.com>
2024-01-20 22:34:56 +0700

    Add addSingleQuestion page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,14 @@
-const path = require('path');
+const path = require("path");
 
 module.exports.home = function (req, res) {
-    res.render('index');
-}
+  res.render("index");
+};
 module.exports.footer = function (req, res) {
-    res.render('footer');
-}
+  res.render("footer");
+};
+module.exports.HieuVV5 = function (req, res) {
+  res.render("HieuVV5");
+};
+module.exports.addSingleQuestion = function (req, res) {
+  res.render("addSingleQuestion");
+};
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -4,11 +4,15 @@ const { CheckLogin } = require("../controllers/authController");
 
 const {
   home,
-  footer
+  footer,
+  HieuVV5,
+  addSingleQuestion,
 } = require("../controllers/homeController");
 
 router.get("/", CheckLogin, home);
 router.get("/home", home);
 router.get("/footer", footer);
+router.get("/HieuVV5", HieuVV5);
+router.get("/addSingleQuestion", addSingleQuestion);
 
 module.exports = router;
